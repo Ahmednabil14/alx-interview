8fba3e873bd6bfb9e7146c1d15a7f802371e567b
ahmednabil14 <ahmednabil14499@gmail.com>
2024-12-05 23:23:39 +0200

    task

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Calculate Island Perimeter"""
+
+
+def island_perimeter(grid):
+    """perimeter of the island described in grid"""
+    borders = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 0:
+                continue
+            borders += 4
+            if i and grid[i - 1][j]:
+                borders -= 2
+            if j and grid[i][j - 1]:
+                borders -= 2
+    return borders
